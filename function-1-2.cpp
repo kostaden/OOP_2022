4bbefbab2d1167be4033e5ce09ebd3d7a0da4a02
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 <kostaden@LAPTOP-BQF3R46E.localdomain>
2022-08-05 16:30:19 +0930

    update

---
+++ function-1-2.cpp
@@ -0,0 +1,20 @@
+// This function checks if a given matrix is an identity matrix. (An identiy
+// matrix is a matrix with 1's down the main diagonal and 0's elsewhere)
+
+#include <iostream>
+
+int is_identity(int array[10][10]) {
+  for (int i = 0; i < 10; i++) {
+    for (int j = 0; j < 10; j++) {
+      if (i == j && array[i][j] != 1) {
+        return 0;
+      }
+      if (i != j && array[i][j] != 0) {
+        return 0;
+      } else {
+        return 1;
+      }
+    }
+  }
+  return 0;
+}
