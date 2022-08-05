e972cf630bd1bc41ede83f573c5d2f1aaacee3a5
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 <kostaden@LAPTOP-BQF3R46E.localdomain>
2022-08-05 20:21:23 +0930

    update

--- function-1-4.cpp
+++ function-1-4.cpp
@@ -4,9 +4,15 @@
 void print_scaled(int array[3][3], int scale) {
   for (int i = 0; i < 3; i++) {
     for (int j = 0; j < 3; j++) {
-      std::cout << array[i][j] * scale << " ";
+      if (j == 0 || j == 1) {
+        std::cout << array[i][j] * scale << " ";
+      }
+      else{
+        std::cout << array[i][j] * scale;
+  }
     }
-    std::cout << std::endl;
+    
+   std::cout << std::endl;
   }
   return;
 }
