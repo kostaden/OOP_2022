0441fd5cd5090b11ff2ac0a5e7da120fe2b7b469
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 <kostaden@LAPTOP-BQF3R46E.localdomain>
2022-08-05 16:36:02 +0930

    update

--- function-1-2.cpp
+++ function-1-2.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 
 int is_identity(int array[10][10]) {
+  int facts = 1;
   for (int i = 0; i < 10; i++) {
     for (int j = 0; j < 10; j++) {
       if (i == j && array[i][j] != 1) {
@@ -11,10 +12,12 @@ int is_identity(int array[10][10]) {
       }
       if (i != j && array[i][j] != 0) {
         return 0;
-      } else {
-        return 1;
       }
     }
   }
-  return 0;
+  if (facts == true) {
+    return 1;
+  } else {
+    return 0;
+  }
 }
