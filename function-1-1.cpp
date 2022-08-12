83d320895ce069067c02218aefa41bea3843d130
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 <kostaden@LAPTOP-BQF3R46E.localdomain>
2022-08-12 14:46:57 +0930

    update

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
+using namespace std;
 
 void print_matrix(int array[10][10]){
 
     for(int i = 0; i < 10; i++){
+        for(int a = 0; a < 10; a++){
+            cout << " " << array[i][a];
+
+        }
+        cout << endl;
         
     }
+    return;
 }
