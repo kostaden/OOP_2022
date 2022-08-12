4b98c8a629782a188f174be67779be9065e771de
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 <kostaden@LAPTOP-BQF3R46E.localdomain>
2022-08-12 14:53:05 +0930

    update

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -5,7 +5,7 @@ void print_matrix(int array[10][10]){
 
     for(int i = 0; i < 10; i++){
         for(int a = 0; a < 10; a++){
-            cout << " " << array[i][a];
+            cout << array[i][a]<< " ";
 
         }
         cout << endl;
