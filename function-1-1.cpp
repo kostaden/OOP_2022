d17962cd1ffec1933e74c87279ebc8dc2c55d7dc
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 <kostaden@LAPTOP-BQF3R46E.localdomain>
2022-08-12 14:54:34 +0930

    update

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -6,7 +6,7 @@ void print_matrix(int array[10][10]){
     for(int i = 0; i < 10; i++){
         for(int a = 0; a < 10; a++){
             cout << array[i][a]<< " ";
-
+        
         }
         cout << endl;
         
