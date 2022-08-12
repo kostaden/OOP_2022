8a092b17ddbcb85c696692de2a843445f1a0a267
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 <kostaden@LAPTOP-BQF3R46E.localdomain>
2022-08-12 15:02:44 +0930

    update

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -6,7 +6,7 @@ void print_matrix(int array[10][10]){
     for(int i = 0; i < 10; i++){
         for(int a = 0; a < 10; a++){
             if(a == 0 || a == 1 || a == 2 || a == 3 || a == 4 || a == 5 || a == 6 || a == 7 || a == 8 || a == 9){
-            cout << array[i][a]<< " ";
+            cout << " " << array[i][a];
             }
             else{
             cout << array[i][a];
