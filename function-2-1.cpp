578b6eebd1ccbd082559c49820094fee0e749d27
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 <kostaden@LAPTOP-BQF3R46E.localdomain>
2022-08-26 15:45:39 +0930

    update

--- function-2-1.cpp
+++ function-2-1.cpp
@@ -4,20 +4,23 @@ using namespace std;
 
 string get_temp_phase(int temp){
 
+    string temp_phase = "";
+
     if(temp <= 273){
-        cout << "ice";
+        temp_phase = "ice";
+        return temp_phase;
     }
     else if(temp > 273 || temp <= 373){
-        cout << "liquid";
+        temp_phase = "liquid";
+        return temp_phase;
     }
-    
-    else if(temp > 373 || temp < 4700){
-        cout << "gas";
-    
+    else if(temp > 373 || temp <= 4700){
+        temp_phase = "gas";
+        return temp_phase;
     }
     else if(temp > 4700){
-        cout << "decomposed";
-        return 0;
+        temp_phase = "decomposed";
+        return temp_phase;
     }
-return 0;
+    return 0;
 }
--- main-2-1.cpp
+++ main-2-1.cpp
@@ -5,14 +5,14 @@ extern string get_temp_phase(int temp);
 
 int main(){
 
-    int temp = 10;
-    cout << "phase at" << temp << "K is: " << get_temp_phase(temp) << endl;
-    temp = 1000;
-    cout << "phase at" << temp << "K is: " << get_temp_phase(temp) << endl;
+    int temp = 1000;
+    cout << "phase at " << temp << "K is: " << get_temp_phase(temp) << endl;
     temp = 273;
-    cout << "phase at" << temp << "K is: " << get_temp_phase(temp) << endl;
-    temp = 373;
-    cout << "phase at" << temp << "K is: " << get_temp_phase(temp) << endl;
+    cout << "phase at " << temp << "K is: " << get_temp_phase(temp) << endl;
+    temp = 293;
+    cout << "phase at " << temp << "K is: " << get_temp_phase(temp) << endl;
+    temp = 10000;
+    cout << "phase at " << temp << "K is: " << get_temp_phase(temp) << endl;
     
     return temp;
     
