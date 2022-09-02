c973c7fc4aa84b279a24863bdd8c78cd2a4e0f58
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 <kostaden@LAPTOP-BQF3R46E.localdomain>
2022-09-02 19:52:50 +0930

    update

--- main-1-2.cpp
+++ main-1-2.cpp
@@ -12,9 +12,9 @@ int main(){
     c.setName("Divya");
     d.setName("Lachlan");
 
-    a.setSalary(20000);
-    b.setSalary(18000);
-    c.setSalary(10000);
+    b.setSalary(20000);
+    c.setSalary(18000);
+    d.setSalary(10000);
     
     cout << a.getName() << a.getSalary() << endl << b.getName() << b.getSalary() << endl << c.getName() << c.getSalary() << endl << d.getName() << d.getSalary() << endl; 
 }
