b7e578e2f885660eea7736c83aa56e3c05cee260
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 <kostaden@LAPTOP-BQF3R46E.localdomain>
2022-09-08 17:48:45 +0930

    update

---
+++ Musician.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include "Musician.h"
+using namespace std;
+
+//Implementation of book constructor
+Musician::Musician(){
+
+name = "";
+age = 0;
+}
+Musician::
+
+void Capybara::setName(string capyName){
+    name = capyName;
+}
+
+void Capybara::setAge(int capyAge){
+    age = capyAge;
+}
+
+string Capybara::getName(){
+    return name;
+}
+
+int Capybara::getAge(){
+    return age;
+}
+
+Musician::~Musician()
+{
+
+}
