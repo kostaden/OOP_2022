b9f5959295c67330a74ac1e91b2cff0a96d04ea3
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 <kostaden@LAPTOP-BQF3R46E.localdomain>
2022-10-28 15:45:04 +1030

    update

--- Car.cpp
+++ Car.cpp
@@ -13,27 +13,30 @@ void Car::set_price(int p){
     price = p;
 }
 
-void Car::set_vinNumber(int vN){
-    vinNumber = vN;
-}
-
-void Car::set_emissions(int em){
-    emissions = em;
-}
-
 int Car::get_price(){
     return price;
+}
+
+void Car::set_vinNumber(int vN){
+    vinNumber = vN;
 }
 
 int Car::get_vinNumber(){
     return vinNumber;
 }
 
+void Car::set_emissions(int em){
+    emissions = em;
+}
+
 int Car::get_emissions(){
     return emissions;
 }
 
+
+
+
 void Car::drive(int kms){
-    
+
 }
 
--- Car.h
+++ Car.h
@@ -16,13 +16,15 @@ class Car{
     Car(int price);
 
     void set_price(int p);
-    void set_vinNumber(int vN);
-    void set_emissions(int em);
-
-    int get_emissions();
     int get_price();
+
+    void set_vinNumber(int vN);
     int get_vinNumber();
 
+    void set_emissions(int em);
+    int get_emissions();
+   
+    
     virtual void drive(int kms) = 0;
 
 
